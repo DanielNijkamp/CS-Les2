ac2774c456a88eb687e38e750ce9cc44c5be52c2
Daniel <danielnijkamp2004@gmail.com>
2021-10-08 09:10:31 +0200

    bruh

---
+++ ArrayOpdracht/ArraysAndDictionaries/ArraysAndDictionaries/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ArraysAndDictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+        public Room(int _width, int _Height)
+        {
+            
+        }
+    }
+    class Room
+    {
+        int[,] m_Array =
+        {
+            {0,0,0},
+            {0,0,0}
+        };
+    }
+}
